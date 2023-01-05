e3f592994b2bd2c4ee47c304f73e78c583c19981
Matthias Brück <hi@brueck.io>
2023-01-05 15:35:46 +0100

    Add hero page

--- brueckio/pages/models.py
+++ brueckio/pages/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+from wagtail.fields import RichTextField, StreamField
 from wagtail.models import Page
-from wagtail.fields import StreamField, RichTextField
-from wagtail.admin.panels import FieldPanel
 from wagtail.snippets.models import register_snippet
 
 from .blocks import BASE_BLOCKS
+
 
 class HomePage(Page):
     parent_page_types = ['wagtailcore.page']
@@ -32,6 +32,23 @@ class ContentPage(Page):
     ]
 
 
+class HeroPage(Page):
+    parent_page_types = ['HomePage']
+    hero_title = RichTextField()
+    hero_text = RichTextField()
+    content = StreamField(BASE_BLOCKS, null=True, blank=True, use_json_field=True)
+
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel('hero_title'),
+                FieldPanel('hero_text'),
+            ]
+        ),
+        FieldPanel('content'),
+    ]
+
+
 class ContactPage(ContentPage):
     pass
 
@@ -46,7 +63,7 @@ class ProjectOverviewPage(Page):
     ]
 
     def get_context(self, value, parent_context=None):
-        context = super(ProjectOverviewPage, self).get_context(value, parent_context=parent_context)
+        context = super().get_context(value, parent_context=parent_context)
         context['projects'] = ProjectPage.objects.live().order_by('-first_published_at')
         return context
 
@@ -65,13 +82,20 @@ class ProjectPage(Page):
     is_highlight = models.BooleanField(default=False)
     description = RichTextField()
     content = StreamField(BASE_BLOCKS, null=True, blank=True, use_json_field=True)
+    contact_banner_text = RichTextField(blank=True, null=True)
     content_panels = Page.content_panels + [
         FieldPanel('is_highlight'),
         FieldPanel('image'),
         FieldPanel('description'),
+        FieldPanel('contact_banner_text'),
         FieldPanel('content'),
     ]
 
+    def get_context(self, value, parent_context=None):
+        context = super().get_context(value, parent_context=parent_context)
+        if self.contact_banner_text:
+            context['contact_banner_text'] = self.contact_banner_text
+        return context
 
 @register_snippet
 class Testimonial(models.Model):
@@ -101,5 +125,3 @@ class Testimonial(models.Model):
 
     def __str__(self):
         return self.name
-
-
