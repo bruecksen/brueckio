7998bef7ae483a60e540961b13e409e0a134788b
Matthias Brück <hi@brueck.io>
2023-01-20 16:04:24 +0100

    Disable error email

--- config/settings/base.py
+++ config/settings/base.py
@@ -227,6 +227,25 @@ ADMINS = [
 # https://docs.djangoproject.com/en/dev/ref/settings/#managers
 MANAGERS = ADMINS
 
+LOGGING = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "verbose": {
+            "format": "%(levelname)s %(asctime)s %(module)s "
+            "%(process)d %(thread)d %(message)s"
+        }
+    },
+    "handlers": {
+        "console": {
+            "level": "DEBUG",
+            "class": "logging.StreamHandler",
+            "formatter": "verbose",
+        }
+    },
+    "root": {"level": "INFO", "handlers": ["console"]},
+}
+
 
 
 # Wagtail settings
